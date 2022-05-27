3e5ad41880d6301ea596ab5df38d1ab6681fcfd3
Viral Tagdiwala <tagdiwalaviral@gmail.com>
2022-05-26 20:29:55 -0700

    Time: 92 ms (95.06%), Space: 50.7 MB (64.26%) - LeetHub

---
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.ts
@@ -0,0 +1,41 @@
+const CHARACTER = 0
+const NUMBER_OF_REPEATS =1
+
+function removeDuplicates(s: string, k: number): string {
+
+    const stack = []
+    stack.push([s[0] , 1])
+    
+    for(let i=1;i<s.length;i++){
+        
+        if(stack.length > 0){
+             const top = stack[stack.length-1]
+        
+            if(top && top[CHARACTER] === s[i]){
+                top[NUMBER_OF_REPEATS]++;
+
+                if(top[NUMBER_OF_REPEATS] === k){
+                    stack.pop()
+                }else{
+                    stack.pop()
+                    stack.push(top)
+                }
+            }else{
+                stack.push([s[i], 1])
+            }
+        }else{
+            stack.push([s[i], 1])
+        }
+       
+    }
+    
+    let newString =""
+    
+    stack.map((character) => {
+        for(let i=0;i<character[NUMBER_OF_REPEATS];i++){
+            newString += character[CHARACTER]
+        }
+    })
+    
+    return newString
+};
