df6d6965a7ed08cde0f635087f35d5ed19bd4104
Viral Tagdiwala <tagdiwalaviral@gmail.com>
2022-06-01 20:06:01 -0700

    Time: 90 ms (52.48%), Space: 44.6 MB (45.39%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.ts
@@ -0,0 +1,22 @@
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
+function rotate(matrix: number[][]): void {
+
+    let left = 0, right = matrix.length -1
+    
+    while(left < right){
+        for(let i=0;i<right-left;i++){
+            let top = left, bottom = right
+            
+            let topLeft = matrix[top][left + i]
+            
+            matrix[top][left + i] = matrix[bottom - i][left]
+            matrix[bottom - i][left] = matrix[bottom][right -i]
+            matrix[bottom][right - i] = matrix[top + i][right]
+            matrix[top + i][right] = topLeft
+        }
+        left ++;
+        right --;
+    }
+};
