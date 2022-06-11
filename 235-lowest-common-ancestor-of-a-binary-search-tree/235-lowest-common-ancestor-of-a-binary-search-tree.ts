cebeaf9ad41e0a9a77ca88256b86f3eaf6e0e769
Viral Tagdiwala <tagdiwalaviral@gmail.com>
2022-06-11 16:07:18 -0700

    Time: 88 ms (92.06%), Space: 53.1 MB (47.62%) - LeetHub

---
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.ts
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
+	let current = root
+    
+    while(current){
+        const currentVal = current.val
+        if(p.val > currentVal && q.val > currentVal){
+            current = current.right
+        }else if(p.val < currentVal && q.val < currentVal){
+            current = current.left
+        }else{
+            return current
+        }
+    }
+};
