2e463d9e98f2c2308a0f30cbf5622ed34dc8b5b6
Viral Tagdiwala <tagdiwalaviral@gmail.com>
2022-06-16 17:35:09 -0700

    Time: 142 ms (36.25%), Space: 52.4 MB (12.86%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.ts
@@ -0,0 +1,9 @@
+function maxProfit(prices: number[]): number {
+    let min = Number.MAX_SAFE_INTEGER; 
+    let max = 0;
+    for (let i = 0; i < prices.length; i++) {
+        min = Math.min(min, prices[i]);
+        max = Math.max(max, prices[i] - min);
+    }
+    return max;
+};
