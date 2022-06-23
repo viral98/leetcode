b1c5f80acb94c077faab9c1397b5d9fa22dac7ac
Viral Tagdiwala <tagdiwalaviral@gmail.com>
2022-06-23 05:21:40 -0700

    Time: 356 ms (59.26%), Space: 51.4 MB (65.19%) - LeetHub

---
+++ 127-word-ladder/127-word-ladder.ts
@@ -0,0 +1,32 @@
+function ladderLength(beginWord: string, endWord: string, wordList: string[]): number {
+    const wordSet = new Set(wordList)
+    let queue = [beginWord];
+    let steps = 1;
+    
+    while(queue.length) {
+        const next = [];
+        
+        // loop over each word in the queue
+        for(let word of queue) {
+            if(word === endWord) return steps;
+            
+            // loop over each char of the word 
+            for(let i = 0; i < word.length; i++) {
+                
+                // and replace the char with letters from [a - z]
+                for(let j = 0; j < 26; j++) {
+                    const newWord = word.slice(0, i) + String.fromCharCode(j + 97) + word.slice(i+1);
+                    
+                    // if the new word exist in the word list add it to the queue
+                    if(wordSet.has(newWord)) {
+                        next.push(newWord);
+                        wordSet.delete(newWord);
+                    }
+                }
+            }
+        }
+        queue = next
+        steps++;
+    }
+    return 0;    
+};
